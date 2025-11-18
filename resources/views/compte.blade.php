b2b7c9d90312c45831f03b2380ab37bd13a41513
kamelia hamchouche <hamchouchek65@gmail.com>
2025-11-18 18:15:41 +0100

    ajout du .composer dans le gitignor et creation des fichiers blades(encore vide)

---
+++ resources/views/compte.blade.php
@@ -0,0 +1,29 @@
+@extends('layouts.app')
+
+{{-- On ajoute le CSS spécifique à cette page (compte.css) --}}
+@push('styles')
+    <link href="{{ asset('ressources/Style/compte.css') }}" rel="stylesheet">
+@endpush
+
+@section('content')
+
+<div class="login-container">
+    {{-- J'ai gardé action="#" pour l'instant, mais tu devras mettre ta route Laravel ici plus tard --}}
+    <form method="POST" action="#">
+        
+        @csrf {{-- PROTECTION OBLIGATOIRE LARAVEL (remplace ton ancienne sécurité) --}}
+        
+        <img class="userIcon" src="{{ asset('ressources/Images/user.png') }}">
+        <input type="hidden" name="action" value="connexionUtilisateur">
+        
+        <p>Nom d'utilisateur :</p>
+        <input type="text" name="loginUser">
+        
+        <p>Mot de passe :</p>
+        <input type="password" name="passwordUser">
+        
+        <button type="submit" class="btn">Confirmer</button>
+    </form>
+</div>
+
+@endsection
