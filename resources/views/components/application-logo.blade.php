54292691f68eb0cd79d39d590e9de50c3af54f8d
liam04041 <liam.lucas414@gmail.com>
2025-11-24 13:50:48 +0100

    Debut de l'adaptation de la navbar : ajout des différents éléments issus de l'app précédente

---
+++ resources/views/components/application-logo.blade.php
@@ -0,0 +1,1 @@
+<img src="{{ asset('images/logo_BTS_PLAY.png') }}" {{ $attributes }} alt="Logo">
---
+++ resources/views/home.blade.php
@@ -0,0 +1,72 @@
+@extends('layouts.app')
+
+{{-- Suppression des balises HTML structurelles (html, head, body) car elles sont gérées par layouts/app.blade.php --}}
+
+{{-- On ajoute les styles et scripts spécifiques à cette page --}}
+@push('styles')
+    <link href="{{ asset('resources/css/menuArbo.css') }}" rel="stylesheet">
+    <link href="{{ asset('resources/css/home.css') }}" rel="stylesheet">
+    <link href="{{ asset('ressources/lib/Swiper/swiper-bundle.min.css') }}" rel="stylesheet">
+@endpush
+
+@section('content')
+
+    {{-- @include('menuArbo') --}}
+
+    <div class="container">
+        <div class="sliderVideo">
+            <h2>Vos vidéos</h2>
+            <div class="swiperVideo">
+                <div class="swiper-wrapper">
+                    @foreach ($medias as $media)
+                        <div class='swiper-slide'>
+                            <a href="{{ route('medias.show', $media->id) }}">
+                                <div class='miniature'>
+                                    <img src="{{ asset($media->cheminMiniatureComplet) }}" alt='Miniature de la vidéo' class='imageMiniature'/>
+                                </div>
+                                <h3>{{ $media->mtd_tech_titre }}</h3>
+                            </a>
+                        </div>
+                    @endforeach
+                </div>
+            </div>
+            <div class="swiper-button-next"></div>
+            <div class="swiper-button-prev"></div>
+        </div>
+
+        {{-- Logique pour afficher le slider du dernier projet --}}
+        @if (!empty($tabDernierProjet))
+            <div class="sliderVideoProjet">
+            <h2>{{ $tabDernierProjet[0]["projet"] }}</h2>
+            <div class="swiperVideo">
+                <div class="swiper-wrapper">
+                    @foreach ($tabDernierProjet as $video)
+                        <div class='swiper-slide'>
+                            <a href="{{ route('medias.show', ['v' => $video['id']]) }}">
+                                <div class='miniature'>
+                                    <img src="{{ asset($video['cheminMiniatureComplet']) }}" alt='Miniature de la vidéo' class='imageMiniature'/>
+                                </div>
+                                <h3>{{ $video['titre'] }}</h3>
+                                <h4>{{ $video['titreVideo'] }}</h4>
+                            </a>
+                        </div>
+                    @endforeach
+                </div>
+            </div>
+            <div class="swiper-projet-button-next"></div>
+            <div class="swiper-projet-button-prev"></div>
+        </div>
+        @endif
+        
+    </div>
+
+@endsection
+
+@push('scripts')
+    <script src="{{ asset('ressources/lib/Swiper/swiper-bundle.min.js') }}"></script>
+    <script>
+        document.addEventListener("DOMContentLoaded", function () {
+            initCarrousel();
+        });
+    </script>
+@endpush
