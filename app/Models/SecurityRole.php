08bb1258d717e2395348d3ae8ffa0a036d6ea270
liam04041 <liam.lucas414@gmail.com>
2026-02-05 14:58:12 +0100

    Ajout du package Spatie au projet pour gérer proprement les regles de sécurité associés aux utilisateurs et installation / configuration initiale. implémentation en attente

---
+++ app/Models/SecurityRole.php
@@ -0,0 +1,10 @@
+<?php
+namespace App\Models;
+
+use Spatie\Permission\Models\Role as SpatieRole;
+
+class SecurityRole extends SpatieRole
+{
+// tu peux ajouter des méthodes custom ici si besoin
+}
+?>
