5f7b4e9caa0ff6957eb6540eaf6aaaaa158977ad
liam04041 <liam.lucas414@gmail.com>
2026-02-06 15:35:51 +0100

    ajout d'un seeder pour les regles de sécurité (compte admin)

--- app/Models/SecurityPermission.php
+++ app/Models/SecurityPermission.php
@@ -5,6 +5,6 @@ use Spatie\Permission\Models\Role as SpatieRole;
 
 class SecurityPermission extends SpatieRole
 {
-// tu peux ajouter des méthodes custom ici si besoin
+
 }
 ?>
--- app/Models/SecurityRole.php
+++ app/Models/SecurityRole.php
@@ -5,6 +5,5 @@ use Spatie\Permission\Models\Role as SpatieRole;
 
 class SecurityRole extends SpatieRole
 {
-// tu peux ajouter des méthodes custom ici si besoin
 }
 ?>
